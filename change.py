9a91cacf27c2e4b5d3f92af0ec15202bdd681dc7
tomasL-20 <tlanusse@mail.austral.edu.ar>
2025-04-02 19:18:28 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    pesos = int(vuelto)
+    centavos = int((vuelto - pesos) * 100)
+    print(f"Ingresar gasto: \n {expense}")
+    print(f"dinero recibido: \n {money}")
+    print(f"Vuelto")
+    print(f"Pesos:\n {pesos}")
+    print(f"Centavos: \n {centavos}")
+
+
+change()
+
